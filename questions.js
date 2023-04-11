d6c5353b3c3e8db861bdb3a137378227295fea46
Yuvraj <yuvrajvarshney2021@gmail.com>
2023-04-11 23:42:51 +0530

    Questions added in questions.js file

---
+++ questions.js
@@ -0,0 +1,115 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  }, 
+   {
+    numb: 6,
+  question: "Which is the correct CSS syntax? ",
+    answer: "body {color: black;}",
+   options: [
+    "body {color: black;}",
+     "{body:color=black;} ",
+     " body:color=black;",
+      " {body;color:black;}"
+   ]
+  },
+  {
+    numb: 7,
+  question: "Which jQuery method is used to hide selected elements? ",
+    answer: "hide()",
+   options: [
+    "hidden()",
+    "visible(false)",
+    "hide()",
+    "display(none)"
+   ]
+  },
+  {
+    numb: 8,
+  question: "How do you add a background color for all h1 elements?",
+    answer: "h1 {background-color:#FFFFFF;}",
+   options: [
+   "h1 {background-color:#FFFFFF;}",
+   " h1.all {background-color:#FFFFFF;}",
+   " all.h1 {background-color:#FFFFFF;}",
+   "#h1 {background-color:#FFFFFF;}"
+
+   ]
+  },
+  {
+    numb: 9,
+  question: "With jQuery, look at the following selector: $(div.intro). What does it select?",
+    answer: "All div elements with class=intro",
+   options: [
+    "The first div element with class=intro",
+    "All div elements with class=intro",
+    "The first div element with id=intro",
+    "All div elements with id=intro"
+
+   ]
+  },
+  {
+    numb: 10,
+  question: "Which sign does jQuery use as a shortcut for jQuery?",
+    answer: "$",
+   options: [
+    "#",
+    "?",
+    "$",
+    "!"
+
+   ]
+  },
+]
